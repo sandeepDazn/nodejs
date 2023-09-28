80f652d80131c3fe090ee1350d5647de739ff7d0
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2023-09-28 22:35:59 +0530

    github actions setup

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/testing", (req, res) => {
-   
   res.send("hello sandeep world");
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome page");
 });
 
 app.listen(8080, () => {
